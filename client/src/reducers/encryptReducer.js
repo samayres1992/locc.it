19bc97c04cabde118a55a7db0ea7c1fed253a93b
samayres1992 <samayres1992@gmail.com>
2019-10-30 21:36:01 -0400

    Encrypt work

--- client/src/reducers/encryptReducer.js
+++ client/src/reducers/encryptReducer.js
@@ -1,8 +1,9 @@
-import { CODE_GEN } from '../actions/types'; 
+import { ENCRYPT } from '../actions/types'; 
 
 export default function (state = null, action) {
   switch (action.type) {
-    case CODE_GEN:
+    case ENCRYPT:
+      console.log('encrypt', action);
       return action.payload || false; 
     default:
       return state;
