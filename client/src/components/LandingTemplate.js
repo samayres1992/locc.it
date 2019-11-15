3592eb1107265dc357f8a378ec239fb0d870651f
samayres1992 <samayres1992@icloud.com>
2019-11-15 16:06:09 -0500

    Moved encrypt form to own component, updates encrypt requests

--- client/src/components/LandingTemplate.js
+++ client/src/components/LandingTemplate.js
@@ -1,67 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Field, reduxForm } from 'redux-form';
-import classNames from 'classnames';
 import * as actions from '../actions';
+import EncryptForm from './EncryptForm';
 
 class LandingTemplate extends Component {
-  inputRender = (field) => (
-    <input {...field.input} name={field.name} className={classNames({"has-content": field.meta.dirty, "fancy-input": !field.dirty})} type={field.type} placeholder="" type="text" required />
-  );
 
-  textareaRender = (field) => (
-    <textarea {...field.input} name={field.name} className={classNames({"has-content": field.meta.dirty, "fancy-input": !field.dirty})} type={field.type} placeholder="" type="text" required />
-  );
+  constructor(props) {
+    super();
+    this.state = {
+      passcode: null
+    };
+  }
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("this.props.encrypt", this.props.encrypt());
+  componentDidUpdate(prevState, prevProps) {
+    if (prevState.passcode !== prevProps.passcode) {
+      this.setState({
+        passcode: this.props.passcode
+      });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    return (nextState.passcode !== this.state.passcode);
   }
 
   render() {
-    const { label, pristine, submitting } = this.props;
+    const { passcode } = this.state;
+    const Display = passcode ? passcode : EncryptForm;
 
     return (
-      <form onSubmit={this.handleSubmit}>
-        <div className="input-effect">
-          <Field name="emailUsername" component={this.inputRender} type="text" />
-            <label>Email/Username</label>
-            <span className="focus-border">
-              <i></i>
-            </span>
-        </div>
-        <div className="input-effect">
-          <Field name="password" component={this.inputRender} type="password" />
-            <label>Password</label>
-            <span className="focus-border">
-              <i></i>
-            </span>
-        </div>
-        <div className="input-effect">
-          <Field name="note" component={this.textareaRender} type="textarea" placeholder="Add a note if you like" />
-            <label>Note</label>
-            <span className="focus-border">
-              <i></i>
-            </span>
-        </div>
-        <button className="submit button fancy-button" disabled={pristine || submitting}>
-          Encrypt
-          <span className="focus-border">
-            <i></i>
-          </span>
-        </button>
-      </form>
+      <div>
+        <Display />
+      </div>
     );
   }
 }
 
-function mapStateToProps({ key }) {
-  return {
-    encrypt: key || null
-  };
-}
-
-export default reduxForm({
-  form: 'form', // a unique identifier for this form
-  fields: ['emailUsername', 'password', 'note'],
-})(connect(mapStateToProps, actions)(LandingTemplate))
+export default connect(null, actions)(LandingTemplate);
--- client/src/reducers/encryptReducer.js
+++ client/src/reducers/encryptReducer.js
@@ -3,8 +3,11 @@ import { ENCRYPT } from '../actions/types';
 export default function (state = null, action) {
   switch (action.type) {
     case ENCRYPT:
-      console.log('encrypt', action);
-      return action.payload || false; 
+      console.log('encrypt', this.state);
+      return {
+        ...state,
+        encrypt: action.payload
+      };
     default:
       return state;
   }
