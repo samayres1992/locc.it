9fc779319285bf5c336e15917e1bfece5a499673
samayres1992 <samayres1992@icloud.com>
2019-12-28 00:01:00 -0500

    Added google analytics

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
+import ReactGA from 'react-ga';
 import 'antd/dist/antd.css'; // https://ant.design/docs/react/introduce
 import 'antd-mobile/dist/antd-mobile.css'; 
 // use gulp to compile
@@ -18,6 +19,9 @@ import App from './App';
 
 // Reducers
 import reducers from './reducers';
+
+ReactGA.initialize('UA-153191251-1');
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 // Only run devtools in dev
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
