b35aa57b8722c04e9c465fc81f7a6e6801748218
Sam Ayres <samayres1992@icloud.com>
2026-06-01 00:16:52 -0400

    Updated var refs

--- server/config/system.js
+++ server/config/system.js
@@ -3,7 +3,7 @@ const path = require('path');
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  BASE_URL: process.env.SITE_URL || process.env.BASE_URL || (isProduction ? 'https://locc.it' : 'http://localhost:3000'),
+  BASE_URL: (process.env.SITE_URL || process.env.BASE_URL || (isProduction ? 'https://locc.it' : 'http://localhost:3000')).replace(/\/$/, ''),
   BUILD: process.env.BUILD || path.join(__dirname, '../../client/build/index.html'),
   ENV: process.env.APP_ENV || (isProduction ? 'production' : 'development'),
   PORT: Number(process.env.PORT || 5000),
